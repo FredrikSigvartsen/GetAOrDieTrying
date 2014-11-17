f78b665ef5b1bbe997e3aa0ce4160d0000cb0aa0
Elias Andreassen Thøgersen <elias3004@msn.com>
2014-11-17 13:31:06 +0100

    webkit, moz, o, ms

--- RentalNetwork/jssheet.js
+++ RentalNetwork/jssheet.js
@@ -7,25 +7,57 @@ setInterval(function() {
 	var elem4 = document.getElementById("bilde4");
 	
 	if(elem4.style.opacity == "1") {
-		elem1.style.transition = "opacity 1.5s linear 0s";
+		elem1.style.WebkitTransition = "opacity 1.5s linear 0s";
+			elem1.style.MozTransition = "opacity 1.5s linear 0s";
+				elem1.style.OTransition = "opacity 1.5s linear 0s";
+					elem1.style.MsTransition = "opacity 1.5s linear 0s";
+						elem1.style.transition = "opacity 1.5s linear 0s";
 		elem1.style.opacity = "1";
-		elem4.style.transition = "opacity 1.5s linear 0s";
+		elem4.style.WebkitTransition = "opacity 1.5s linear 0s";
+			elem4.style.MozTransition = "opacity 1.5s linear 0s";
+				elem4.style.OTransition = "opacity 1.5s linear 0s";
+					elem4.style.MsTransition = "opacity 1.5s linear 0s";
+						elem4.style.transition = "opacity 1.5s linear 0s";
 		elem4.style.opacity = "0";
 	} else if(elem3.style.opacity == "1") {
-		elem4.style.transition = "opacity 1.5s linear 0s";
+		elem4.style.WebkitTransition = "opacity 1.5s linear 0s";
+			elem4.style.MozTransition = "opacity 1.5s linear 0s";
+				elem4.style.OTransition = "opacity 1.5s linear 0s";
+					elem4.style.MsTransition = "opacity 1.5s linear 0s";
+						elem4.style.transition = "opacity 1.5s linear 0s";
 		elem4.style.opacity = "1";
-		elem3.style.transition = "opacity 1.5s linear 0s";
+		elem3.style.WebkitTransition = "opacity 1.5s linear 0s";
+			elem3.style.MozTransition = "opacity 1.5s linear 0s";
+				elem3.style.OTransition = "opacity 1.5s linear 0s";
+					elem3.style.MsTransition = "opacity 1.5s linear 0s";
+						elem3.style.transition = "opacity 1.5s linear 0s";
 		elem3.style.opacity = "0";
 	} else if(elem2.style.opacity == "1") {
-		elem3.style.transition = "opacity 1.5s linear 0s";
+		elem3.style.WebkitTransition = "opacity 1.5s linear 0s";
+			elem3.style.MozTransition = "opacity 1.5s linear 0s";
+				elem3.style.OTransition = "opacity 1.5s linear 0s";
+					elem3.style.MsTransition = "opacity 1.5s linear 0s";
+						elem3.style.transition = "opacity 1.5s linear 0s";
 		elem3.style.opacity = "1";
-		elem2.style.transition = "opacity 1.5s linear 0s";
+		elem2.style.WebkitTransition = "opacity 1.5s linear 0s";
+			elem2.style.MozTransition = "opacity 1.5s linear 0s";
+				elem2.style.OTransition = "opacity 1.5s linear 0s";
+					elem2.style.MsTransition = "opacity 1.5s linear 0s";
+						elem2.style.transition = "opacity 1.5s linear 0s";
 		elem2.style.opacity = "0";
 	} else {
-		elem1.style.transition = "opacity 1.5s linear 0s";
-		elem1.style.opacity = "0";
-		elem2.style.transition = "opacity 1.5s linear 0s";
+		elem2.style.WebkitTransition = "opacity 1.5s linear 0s";
+			elem2.style.MozTransition = "opacity 1.5s linear 0s";
+				elem2.style.OTransition = "opacity 1.5s linear 0s";
+					elem2.style.MsTransition = "opacity 1.5s linear 0s";
+						elem2.style.transition = "opacity 1.5s linear 0s";
 		elem2.style.opacity = "1";
+		elem1.style.WebkitTransition = "opacity 1.5s linear 0s";
+			elem1.style.MozTransition = "opacity 1.5s linear 0s";
+				elem1.style.OTransition = "opacity 1.5s linear 0s";
+					elem1.style.MsTransition = "opacity 1.5s linear 0s";
+						elem1.style.transition = "opacity 1.5s linear 0s";
+		elem1.style.opacity = "0";
 	}
 	
 },4000);
